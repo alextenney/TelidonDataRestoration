a06d4b60a86a39b16194ff5175602f0ab753155f
Alex Tenney <alexandra.tenney@ucalgary.ca>
2023-02-01 12:34:31 -0700

    a working marking version, isnt removing as much as I'd like. Needs more tests

--- Marking/binary_marking.py
+++ Marking/binary_marking.py
@@ -1,44 +1,52 @@
 import subprocess
-import shutil
+import os
 
 def search_file(string, original_binary, new_binary):
-    identifier = bytes(string, 'utf-8')
+    identifier = string
     replace_string = bytes(' ' * len(identifier), 'utf-8')
-
-    fout = open(new_binary, "wb")
-
+    
     with open(original_binary, 'rb') as f:
-        for line in f.readlines():
+        lines = f.readlines()
+        
+    with open(new_binary, 'wb') as fout:
+        for line in lines:
             if identifier in line:
-                # To do:
-                # figure out how to output the parts of the binary????
-                print("Found in file")
+                # print("Found in file")
                 line = line.replace(identifier, b'')
-                
             fout.write(line)
+
+directories = ["G0001554_A", "G0001554_B", "G0001554_C", "G0001554_D", "G0001555_A", "G0001555_B", "G0001556_A", "G0001556_B", "G0001557", "G0001558"]
+
+def main():
+    src = "/home/alex/Documents/W2023/telidon/LAC/P11191"
+    dst = "/home/alex/Documents/W2023/telidon/LAC/marked_binary"
+    main_path = "/home/alex/Documents/W2023/telidon/LAC/output/"
+
+    # runtime = n^2
+    # maybe see if I can change this
+
+    first_pass = True
+    i = 0
+    for foldername in directories:
+        files = os.listdir(main_path + foldername)
+        for filename in files:
+            f = os.path.join(main_path + foldername, filename)
+            if os.path.isfile(f):
+                # print("Using file: " + f)
+                with open(f, 'rb') as f_search:
+                    search_string = f_search.read()
                 
-                # break
-   
-    fout.close()
+                if first_pass:
+                    search_file(search_string, src, dst)
+                else:
+                    search_file(search_string, dst, dst)
 
-# function to loop through all the files of Aycock's output, and grab
-# that string
-
-#def grab_string():
-
+                first_pass = False
+                i += 1
+            
+            print(f"{i}/{len(files)}", end="\r")
+        print()
+        i = 0
 
 if __name__ == '__main__':
-    # make a duplicate of the original binary file to ensure there is no changes to the original 
-    # if the string is found, it will replace the characters with blank spaces
-    # in the end I will only be left with parts of the binary I could not match
-
-    # if file does not exist, make it
-    # src = "/home/alex/Documents/W2023/telidon/LAC/P11191"
-    src = "/home/alex/Documents/W2023/telidon/output/test"
-    dst = "/home/alex/Documents/W2023/telidon/LAC/marked_binary"
-
-    # else, throw an error and exit 
-    # dont make a billion files I guess
-
-    search_string = """  =@ {O!H@@I<v"@@V"@@@Ma$BLVT$B\FE$BdvL$BtVI$B|vD$JLnO$J\^N<I"@@@IDK$BKjNEWS UPDATE<v"@@@Ma$AOQT$A_AE$AgqL$AwQI$AqD$IOiO$I_YN<I"@@@IDK$AMmNEWS UPDATE<v"@@@Ma$AITT$AYDE$AatL$AqTI$AytD$IIlO$IY\N<I"@@@IDK$AHhNEWS UPDATE<v"@@@Ma$@KWT$@[GE$@cwL$@sWI$@{wD$HKoO$H[_N<I"@@@IDK$@JkNEWS UPDATE+Z|rhh@JcrPX@IfuPX@IeuPX@IapPX@I`pPX@HcsPX@HbsPX@3Jpvxx]<"@@@ArL$Jp~ TELIDON REPORTS$IsH2 TELIDON NEWSLETTERS"@@@KA"@@@ArL$HuI3 TELIDON FORMS"@@@IDK$IrM"""
-    search_file(search_string, src, dst)
+    main()
